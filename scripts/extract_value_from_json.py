207bd3b44d6e29f6517e37e03270998258c70c48
kopardev <vishal.koparde@nih.gov>
2021-07-15 15:37:41 -0400

    extract value from JSON/YAML scripts

---
+++ scripts/extract_value_from_json.py
@@ -0,0 +1,8 @@
+import json
+import argparse
+parser = argparse.ArgumentParser(description='extract value for key from JSON')
+parser.add_argument('-j',dest="json",required=True,help="input JSON file")
+parser.add_argument('-k',dest="key",required=True,help="key whose value is to be extracted")
+args = parser.parse_args()
+data = json.load(open(args.json))
+print(data[args.key])
