f1d007f85137419995d5f5fc2cfbaf91aecbe42e
Kelly Sovacool <kelly.sovacool@nih.gov>
2024-08-07 13:50:33 -0400

    refactor: create main functions for python scripts

--- src/ccbr_tools/homologfinder/hf.py
+++ src/ccbr_tools/homologfinder/hf.py
@@ -12,91 +12,118 @@ Example:
     $ hf -f genelist.txt
 """
 
-__version__ = 'v1.0.0'
-__author__ = 'Vishal Koparde'
-__email__ = 'vishal.koparde@nih.gov'
+__version__ = "v1.0.0"
+__author__ = "Vishal Koparde"
+__email__ = "vishal.koparde@nih.gov"
 
-import argparse,subprocess,json,os,datetime,time,textwrap,sys
+import argparse, subprocess, json, os, datetime, time, textwrap, sys
 import pandas as pd
 import requests
 import io
 
+
 def exit_w_msg(message):
-    """ Gracefully exit with proper messsage"""
-    print('{} : EXITING!!'.format(__file__))
+    """Gracefully exit with proper message"""
+    print("{} : EXITING!!".format(__file__))
     print(message)
     sys.exit()
 
+
 def check_help(parser):
     """check if usage needs to be printed"""
-    if '-h' in sys.argv or '--help' in sys.argv or len(sys.argv) == 1:
+    if "-h" in sys.argv or "--help" in sys.argv or len(sys.argv) == 1:
         print(__doc__)
         parser.print_help()
         parser.exit()
     return
 
+
 def collect_args():
     """collect all the cli arguments"""
-   # create parser
-    parser = argparse.ArgumentParser(description = 'Get Human2Mouse (or Mouse2Human) homolog gene or genelist')
+    # create parser
+    parser = argparse.ArgumentParser(
+        description="Get Human2Mouse (or Mouse2Human) homolog gene or genelist"
+    )
 
     # add version
-    parser.add_argument('-v','--version', action = 'version', version='%(prog)s {}'.format(__version__))
+    parser.add_argument(
+        "-v", "--version", action="version", version="%(prog)s {}".format(__version__)
+    )
 
     # add joblist
-    parser.add_argument('-g','--gene', help='single gene name', required = False, type=str)
+    parser.add_argument(
+        "-g", "--gene", help="single gene name", required=False, type=str
+    )
 
     # add snakemakelog
-    parser.add_argument('-l','--genelist', help='comma separated gene list',required = False, type=str)
+    parser.add_argument(
+        "-l", "--genelist", help="comma separated gene list", required=False, type=str
+    )
 
     # output file
-    parser.add_argument('-f','--genelistfile', help='genelist in file (one gene per line)', type=str, required=False)
-    
+    parser.add_argument(
+        "-f",
+        "--genelistfile",
+        help="genelist in file (one gene per line)",
+        type=str,
+        required=False,
+    )
+
     check_help(parser)
 
     # extract parsed arguments
     args = parser.parse_args()
 
-    if (args.gene and args.genelist) or (args.gene and args.genelistfile) or (args.genelist and args.genelistfile) or (args.gene and args.genelist and args.genelistfile):
+    if (
+        (args.gene and args.genelist)
+        or (args.gene and args.genelistfile)
+        or (args.genelist and args.genelistfile)
+        or (args.gene and args.genelist and args.genelistfile)
+    ):
         msg = "Only one can be provided -g or -l or -f"
         exit_w_msg(msg)
-    
+
     return args
 
-def process_genelist(gl,lookup):
+
+def process_genelist(gl, lookup):
     result = []
     for g in gl:
         if g in lookup:
             result.extend(lookup[g].split(","))
     return result
 
-def process_args(args,lookup):
+
+def process_args(args, lookup):
     if args.gene:
-        r = process_genelist([args.gene],lookup)
+        r = process_genelist([args.gene], lookup)
     if args.genelist:
         gl = args.genelist
-        r = process_genelist(gl.split(","),lookup)
+        r = process_genelist(gl.split(","), lookup)
     if args.genelistfile:
         with open(args.genelistfile) as f:
             lines = f.readlines()
-        lines = list(map(lambda x:x.strip(),lines))
-        r = process_genelist(lines,lookup)
+        lines = list(map(lambda x: x.strip(), lines))
+        r = process_genelist(lines, lookup)
     return r
+
 
 def print_results(result):
     for g in result:
         print(g)
+
 
 def read_lookup():
     lookup = dict()
     # read in lookup table from github
     url = "https://raw.githubusercontent.com/CCBR/Tools/master/homologfinder/human_mouse_homolog_lookup.txt"
     download = requests.get(url).content
-    lookupdf = pd.read_csv(io.StringIO(download.decode('utf-8')),sep="\t")
-    lookupdf.columns = ["geneName","homologs"]
+    lookupdf = pd.read_csv(io.StringIO(download.decode("utf-8")), sep="\t")
+    lookupdf.columns = ["geneName", "homologs"]
     for index, row in lookupdf.iterrows():
-        lookup[row["geneName"]]=row["homologs"]
+        lookup[row["geneName"]] = row["homologs"]
     return lookup
+
 
 def main():
     # collect all arguments
@@ -104,8 +131,9 @@ def main():
     # now that args are correct load in the lookup
     lookup = read_lookup()
     # process the arguments
-    result = process_args(args,lookup)
+    result = process_args(args, lookup)
     print_results(result)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
