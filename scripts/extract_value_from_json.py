d7070a20c991e85a8696a701eb0ff7b359f26c4f
Kelly Sovacool <kelly.sovacool@nih.gov>
2024-08-08 16:12:09 -0400

    fix: syntax for defining scripts
    also remove subdir from scripts

--- scripts/extract_value_from_json.py
+++ scripts/extract_value_from_json.py
@@ -1,8 +1,11 @@
 import json
 import argparse
-parser = argparse.ArgumentParser(description='extract value for key from JSON')
-parser.add_argument('-j',dest="json",required=True,help="input JSON file")
-parser.add_argument('-k',dest="key",required=True,help="key whose value is to be extracted")
+
+parser = argparse.ArgumentParser(description="extract value for key from JSON")
+parser.add_argument("-j", dest="json", required=True, help="input JSON file")
+parser.add_argument(
+    "-k", dest="key", required=True, help="key whose value is to be extracted"
+)
 args = parser.parse_args()
 data = json.load(open(args.json))
 print(data[args.key])
--- scripts/filter_bam_by_readids.py
+++ scripts/filter_bam_by_readids.py
@@ -2,31 +2,43 @@ import pysam
 import sys
 import argparse
 import os
-parser = argparse.ArgumentParser(description='Filter BAM by readids')
-parser.add_argument('--inputBAM', dest='inputBAM', type=str, required=True,
-                    help='input BAM file')
-parser.add_argument('--outputBAM', dest='outputBAM', type=str, required=True,
-                    help='filtered output BAM file')
-parser.add_argument('--readids', dest='readids', type=str, required=True,
-                    help='file with readids to keep (one readid per line)')
+
+parser = argparse.ArgumentParser(description="Filter BAM by readids")
+parser.add_argument(
+    "--inputBAM", dest="inputBAM", type=str, required=True, help="input BAM file"
+)
+parser.add_argument(
+    "--outputBAM",
+    dest="outputBAM",
+    type=str,
+    required=True,
+    help="filtered output BAM file",
+)
+parser.add_argument(
+    "--readids",
+    dest="readids",
+    type=str,
+    required=True,
+    help="file with readids to keep (one readid per line)",
+)
 args = parser.parse_args()
-rids = list(map(lambda x:x.strip(),open(args.readids,'r').readlines()))
+rids = list(map(lambda x: x.strip(), open(args.readids, "r").readlines()))
 inBAM = pysam.AlignmentFile(args.inputBAM, "rb")
 outBAM = pysam.AlignmentFile(args.outputBAM, "wb", template=inBAM)
 bigdict = dict()
 
-count=0
+count = 0
 for read in inBAM.fetch():
-	count+=1
-	if count%1000000 == 0:
-		print("%d reads read!"%(count))
-	qn=read.query_name
-	if not qn in bigdict:
-		bigdict[qn]=list()
-	bigdict[qn].append(read)
+    count += 1
+    if count % 1000000 == 0:
+        print("%d reads read!" % (count))
+    qn = read.query_name
+    if not qn in bigdict:
+        bigdict[qn] = list()
+    bigdict[qn].append(read)
 inBAM.close()
 
 for r in rids:
-	for read in bigdict[r]:
-		outBAM.write(read)
+    for read in bigdict[r]:
+        outBAM.write(read)
 outBAM.close()
