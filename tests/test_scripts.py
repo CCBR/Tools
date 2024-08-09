3f90b07a3f43db5cf242029a44dd36571347c5bd
Kelly Sovacool <kelly.sovacool@nih.gov>
2024-08-09 12:32:29 -0400

    test: fix which_vpn extension
    never trust CoPilot blindly

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -8,7 +8,7 @@ def test_scripts_help():
 
 
 def test_which_vpn():
-    which_vpn = shell_run("which_vpn.py")
+    which_vpn = shell_run("which_vpn.sh")
     assert (
         "Are you really connected to VPN?? Doesn't look like it!" in which_vpn
         or "Your VPN IP is" in which_vpn
