17e05a1f628ff7f5d4d361d4a8489ba36c49fa01
Kelly Sovacool <kelly.sovacool@nih.gov>
2024-08-13 17:20:59 -0400

    test: fix which_vpn test for biowulf

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -1,4 +1,5 @@
 from ccbr_tools.shell import shell_run
+from ccbr_tools.pipeline.util import get_hpcname
 
 
 def test_scripts_help():
@@ -9,7 +10,11 @@ def test_scripts_help():
 
 def test_which_vpn():
     which_vpn = shell_run("which_vpn.sh")
-    assert (
-        "Are you really connected to VPN?? Doesn't look like it!" in which_vpn
-        or "Your VPN IP is" in which_vpn
-    )
+    hpc = get_hpcname()
+    if hpc == "biowulf":
+        assert 'DO NOT RUN THIS ON a BIOWULF interactive node!' in which_vpn
+    else:
+        assert (
+            "Are you really connected to VPN?? Doesn't look like it!" in which_vpn
+            or "Your VPN IP is" in which_vpn
+        )
