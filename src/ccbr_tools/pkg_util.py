3eca6e4effd0c59be21490f5689eb788326d9588
Kelly Sovacool <kelly.sovacool@nih.gov>
2024-08-16 17:52:50 -0400

    docs: rough docstrings for pkg_util

--- src/ccbr_tools/pkg_util.py
+++ src/ccbr_tools/pkg_util.py
@@ -10,6 +10,19 @@ from time import localtime, strftime
 import tomllib
 
 
+"""
+A custom subclass of `click.Group` that provides additional functionality.
+
+Attributes:
+    epilog (str): The epilog text to be displayed in the help message.
+
+Methods:
+    format_epilog(ctx, formatter): Formats the epilog text and writes it to the formatter.
+    list_commands(ctx): Returns a list of subcommands in the order they were added.
+
+"""
+
+
 class CustomClickGroup(click.Group):
     def format_epilog(self, ctx, formatter):
         if self.epilog:
@@ -23,17 +36,29 @@ class CustomClickGroup(click.Group):
 
 
 def repo_base(path=__file__, *paths):
-    """Get the absolute path to a file in the repository
-    @return abs_path <str>
+    """
+    Get the absolute path to a file in the repository
+
+    Args:
+        path (str, optional): The path to the file. Defaults to __file__.
+        *paths (str): Additional paths to join with the base path.
+
+    Returns:
+        str: The absolute path to the file in the repository.
     """
     basedir = pathlib.Path(path).absolute().parent
     return basedir.joinpath(*paths)
 
 
 def get_version(debug=False, repo_base=repo_base):
-    """Get the current version of the ccbr_tools package
-    @param pkg_name <str> : name of the package (default: ccbr_tools)
-    @return version <str>
+    """
+    Get the current version of the ccbr_tools package.
+
+    Args:
+        pkg_name (str): Name of the package (default: ccbr_tools).
+
+    Returns:
+        str: The version of the package.
     """
     version_path = repo_base("VERSION")
     if debug:
@@ -43,17 +68,27 @@ def get_version(debug=False, repo_base=repo_base):
 
 
 def get_package_version(pkg_name="ccbr_tools"):
-    """Get the current version of a package from the metadata
-    @param pkg_name <str> : name of the package (default: ccbr_tools)
-    @return version <str>
+    """
+    Get the current version of a package from the metadata.
+
+    Args:
+        pkg_name (str): Name of the package (default: ccbr_tools).
+
+    Returns:
+        str: The version of the package.
     """
     importlib.metadata.metadata(pkg_name)["Version"]
 
 
 def get_pyproject_toml(pkg_name="ccbr_tools", repo_base=repo_base):
-    """Get the contents of the package's pyproject.toml file
-    @param pkg_name <str> : name of the package (default: ccbr_tools)
-    @return pyproject_toml <dict>
+    """
+    Get the contents of the package's pyproject.toml file.
+
+    Args:
+        pkg_name (str): Name of the package (default: ccbr_tools).
+
+    Returns:
+        dict: The contents of the pyproject.toml file.
     """
     with open(repo_base("pyproject.toml"), "rb") as infile:
         toml_dict = tomllib.load(infile)
@@ -62,7 +97,13 @@ def get_pyproject_toml(pkg_name="ccbr_tools", repo_base=repo_base):
 
 def get_project_scripts(pkg_name="ccbr_tools"):
     """
-    Get list of CLI tools in the package
+    Get a list of CLI tools in the package.
+
+    Args:
+        pkg_name (str): The name of the package. Defaults to "ccbr_tools".
+
+    Returns:
+        list: A sorted list of CLI tool names.
     """
     return sorted(get_pyproject_toml(pkg_name=pkg_name)["project"]["scripts"].keys())
 
@@ -74,21 +115,41 @@ def get_external_scripts(pkg_name="ccbr_tools"):
     list(get_pyproject_toml(pkg_name=pkg_name)["tool"]["setuptools"]["script-files"])
 
 
-def print_citation(
-    citation_file=repo_base("CITATION.cff"),
-    output_format="bibtex",
-):
+def print_citation(citation_file=repo_base("CITATION.cff"), output_format="bibtex"):
+    """
+    Prints the citation for the given citation file in the specified output format.
+
+    Args:
+        citation_file (str): The path to the citation file.
+        output_format (str): The desired output format for the citation.
+
+    Returns:
+        None
+    """
     citation = create_citation(citation_file, None)
     # click.echo(citation._implementation.cffobj['message'])
     validate_or_write_output(None, output_format, False, citation)
 
 
 def msg(err_message):
+    """
+    Prints the error message with a timestamp.
+    Args:
+        err_message (str): The error message to be printed.
+    Returns:
+        None
+    """
     tstamp = strftime("[%Y:%m:%d %H:%M:%S] ", localtime())
     click.echo(tstamp + err_message, err=True)
 
 
 def msg_box(splash, errmsg=None):
+    """
+    Displays a message box with a given splash message.
+    Args:
+        splash (str): The splash message to be displayed.
+        errmsg (str, optional): An error message to be displayed below the splash message. Defaults to None.
+    """
     msg("-" * (len(splash) + 4))
     msg(f"| {splash} |")
     msg(("-" * (len(splash) + 4)))
