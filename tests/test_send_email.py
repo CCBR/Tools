4b65abb466c94e5147fb8fe6bd9ecb975765c45b
Kelly Sovacool <kelly.sovacool@nih.gov>
2024-12-19 12:59:05 -0500

    test: improve test coverage

--- tests/test_send_email.py
+++ tests/test_send_email.py
@@ -1,7 +1,7 @@
 from ccbr_tools.send_email import send_email_msg
 
 
-def test_send_email():
+def test_send_email_no_attach():
     assert {
         "Subject": "test email from python",
         "From": "${USER}@hpc.nih.gov",
@@ -10,3 +10,17 @@ def test_send_email():
         "Content-Transfer-Encoding": "7bit",
         "MIME-Version": "1.0",
     } == dict(send_email_msg(text="hello world", debug=True))
+
+
+def test_send_email_attach():
+    assert {
+        "Subject": "test email from python",
+        "From": "${USER}@hpc.nih.gov",
+        "To": "${USER}@hpc.nih.gov",
+        "Content-Type": "multipart/mixed",
+        "MIME-Version": "1.0",
+    } == dict(
+        send_email_msg(
+            text="hello world", attach_html="tests/data/file.txt", debug=True
+        )
+    )
