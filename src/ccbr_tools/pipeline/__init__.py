440df7358e3ea7b7a115a6b7c989224f0a8db306
Kelly Sovacool <kelly.sovacool@nih.gov>
2024-12-30 16:13:01 -0500

    docs: fix interlinking

--- src/ccbr_tools/pipeline/__init__.py
+++ src/ccbr_tools/pipeline/__init__.py
@@ -1,3 +1,8 @@
 """
 Helpers for bioinformatics pipelines
+
+- [](`~ccbr_tools.pipeline.cache`)
+- [](`~ccbr_tools.pipeline.hpc`)
+- [](`~ccbr_tools.pipeline.nextflow`)
+- [](`~ccbr_tools.pipeline.util`)
 """
