97c428155a2bdd1bd916b58da098385d8e027dd9
Kelly Sovacool <kelly.sovacool@nih.gov>
2024-12-31 14:06:45 -0500

    test: gb2gtf with normal record

--- tests/test_gb2gtf.py
+++ tests/test_gb2gtf.py
@@ -1,6 +1,7 @@
 import pytest
 
 import ccbr_tools.gb2gtf as gb2gtf
+from ccbr_tools.shell import exec_in_context
 
 
 def test_check_args():
@@ -17,3 +18,16 @@ def test_gb2gtf_err():
     with pytest.raises(SystemExit) as exc_info:
         gb2gtf.gb2gtf(["", "tests/data/sequence_U49845.gb"])
     assert str(exc_info.value) == "Something fishy!"
+
+
+def test_gb2gtf():
+    out = exec_in_context(gb2gtf.gb2gtf, ["", "tests/data/sequence_AF165912.gb"])
+    with open("tests/data/snapshots/sequence_AF165912.gtf", "r") as f:
+        gtf = f.read()
+    assert out == gtf
+
+
+if __name__ == "__main__":
+    out = exec_in_context(gb2gtf.gb2gtf, ["", "tests/data/sequence_AF165912.gb"])
+    with open("tests/data/snapshots/sequence_AF165912.gtf", "w") as f:
+        f.write(out)
