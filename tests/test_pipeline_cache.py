a41a8e28d3ab1fd8538a46b361e68437236b33c5
Kelly Sovacool <kelly.sovacool@nih.gov>
2024-12-31 14:53:07 -0500

    test: improve test coverage for pipeline module

--- tests/test_pipeline_cache.py
+++ tests/test_pipeline_cache.py
@@ -1,4 +1,13 @@
-from ccbr_tools.pipeline.cache import get_sif_cache_dir, get_singularity_cachedir
+import argparse
+import pathlib
+import tempfile
+
+from ccbr_tools.pipeline.cache import (
+    get_sif_cache_dir,
+    get_singularity_cachedir,
+    image_cache,
+    check_cache,
+)
 
 
 def test_get_sif_cache_dir():
@@ -17,3 +26,24 @@ def test_get_singularity_cachedir():
     ]
     errors = [assertion for assertion in assertions if not eval(assertion)]
     assert not errors, "errors occurred:\n{}".format("\n".join(errors))
+
+
+def test_image_cache():
+    d = image_cache(
+        argparse.Namespace(output="tests/data", sif_cache="tests/data/sif/"),
+        config=dict(),
+    )
+    assertions = ["images" in d, list(d["images"].values())[0].startswith("docker://")]
+    assert all(assertions)
+
+
+def test_check_cache():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        sif_dir = pathlib.Path(tmpdir) / "sifs"
+        assert all(
+            [
+                check_cache(argparse.Namespace(), sif_dir) == sif_dir,
+                check_cache(argparse.Namespace(), "tests/data/sifs")
+                == "tests/data/sifs",
+            ]
+        )
