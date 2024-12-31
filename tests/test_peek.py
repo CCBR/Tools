441bd9a558f4e1426a29483f0d643c1f98a2533b
Kelly Sovacool <kelly.sovacool@nih.gov>
2024-12-31 11:54:17 -0500

    test: simple test for peek

---
+++ tests/test_peek.py
@@ -0,0 +1,7 @@
+import ccbr_tools.peek as peek
+from ccbr_tools.shell import exec_in_context
+
+
+def test_peek():
+    out = exec_in_context(peek.peek, "tests/data/tab.tsv", 40)
+    assert "tests/data/tab.tsv" in out
