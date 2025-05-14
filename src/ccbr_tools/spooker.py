c36289f5e477c03a8e30ea3d4d3a4d6a054c7b62
Kelly Sovacool <kelly.sovacool@nih.gov>
2025-05-14 11:19:11 -0400

    feat: parse tree with ast when json fails; get size from tree or du

--- src/ccbr_tools/spooker.py
+++ src/ccbr_tools/spooker.py
@@ -7,6 +7,7 @@ It collects metadata about the pipeline run, bundles it into a tarball, and save
 Run `spooker --help` for more information.
 """
 
+import ast
 import click
 import datetime
 import glob
@@ -17,6 +18,7 @@ import os
 import pathlib
 import shutil
 import tarfile
+import warnings
 
 from .shell import shell_run
 from .pipeline import count_samples
@@ -94,25 +96,27 @@ def spooker(
             f"Pipeline output directory does not exist: {pipeline_outdir}"
         )
 
-    # metadata
-    metadata = collect_metadata(
-        pipeline_outdir, pipeline_version, pipeline_name, pipeline_path
-    )
-    timestamp = metadata["date"]
-
+    metadata = {}
     # tree json
     tree_str = get_tree(pipeline_outdir)
-    metadata["tree_json"] = tree_str
+    metadata["outdir_tree"] = tree_str
 
-    # TODO: determine nsamples, different logic for each pipeline
-    nsamples = count_samples(tree_str, pipeline_name)
-    # tree_json = json.loads(tree_str)
+    # pipeline metadata
+    metadata["pipeline_metadata"] = collect_metadata(
+        pipeline_outdir, pipeline_version, pipeline_name, pipeline_path, tree_str
+    )
+    timestamp = metadata["pipeline_metadata"]["date"]
 
     # jobby json
     log_file = glob_files(
         pipeline_outdir, patterns=["snakemake.log", ".nextflow.log"]
     ).pop()
-    metadata["jobby_json"] = json.dumps(jobby([log_file, "--json"]))
+    metadata["jobby"] = json.dumps(jobby([log_file, "--json"]))
+
+    # TODO master job log
+    metadata["master_job_log"] = {"txt": None}
+    # TODO failed jobs logfiles
+    metadata["failed_jobs"] = {}
 
     # write metadata to json
     meta_outfilename = pipeline_outdir / f"{timestamp}.json.gz"
@@ -123,7 +127,7 @@ def spooker(
     hpc = Cluster.create_hpc(debug=debug)
     spook_outfilename = hpc.spook(
         file=meta_outfilename,
-        subdir=f"{get_random_string()}_{metadata['user']}_{timestamp}",
+        subdir=f"{get_random_string()}_{metadata['pipeline_metadata']['user']}_{timestamp}",
     )
 
     # optional cleanup
@@ -138,6 +142,7 @@ def collect_metadata(
     pipeline_name: str,
     pipeline_path: str,
     timestamp=get_timestamp(),
+    tree_str="{}",
 ):
     """
     Collect metadata for the pipeline run and save it to a file.
@@ -147,28 +152,40 @@ def collect_metadata(
         pipeline_version (str): The version of the pipeline.
         pipeline_name (str): The name of the pipeline.
         pipeline_path (str): The path to the pipeline source.
+        timestamp (str, optional): The timestamp of the pipeline run. Defaults to using `~ccbr_tools.pkg_util.get_timestamp()`.
     """
-    outdir_size = shell_run(f"du -bs {pipeline_outdir}").split("\t")[0]
+    tree_dict = load_tree(tree_str)
+    outdir_size = get_disk_usage(tree_dict, pipeline_outdir)
     ccbrpipeliner_version = parse_modules(list_modules()).get(
         "ccbrpipeliner", "unknown"
     )
     groups = shell_run("groups").strip()
+
+    # TODO: determine nsamples, different logic for each pipeline
+    nsamples = count_samples(tree_str, pipeline_name)
+
     metadata = {
-        "pipeline_outdir": str(pipeline_outdir),
-        "pipeline_outdir_size": outdir_size,
         "pipeline_name": pipeline_name,
         "pipeline_path": str(pipeline_path),
+        "pipeline_outdir": str(pipeline_outdir),
+        "pipeline_outdir_size": outdir_size,
         "pipeline_version": pipeline_version,
         "ccbrpipeliner_module": ccbrpipeliner_version,
         "user": os.environ.get("USER"),
+        "uid": os.environ.get("UID"),
+        "groups": groups,
         "date": timestamp,
+        "nsamples": nsamples,
     }
     return metadata
 
 
-def get_tree(pipeline_outdir, args="-aJ"):
+def get_tree(pipeline_outdir, args="-aJ --du"):
     """
     Generate a directory tree structure using the `tree` command-line utility
+
+    Note: when using -J with --du, the output is not valid JSON due to extra trailing commas.
+    It can be parsed with `ast.literal_eval` rather than `json.loads`.
 
     Args:
         pipeline_outdir (str): The path to the directory for which the tree
@@ -180,7 +197,73 @@ def get_tree(pipeline_outdir, args="-aJ"):
         str: The directory tree structure as a string, stripped of any
         leading or trailing whitespace.
     """
-    return shell_run(f"tree {pipeline_outdir} {args}").strip()
+    return shell_run(f"tree {args} {pipeline_outdir}").strip()
+
+
+def load_tree(tree_str):
+    """
+    Load a tree structure from a string, attempting to parse it as JSON or
+    Python literal.
+
+    Args:
+        tree_str (str): The string representation of the tree structure.
+    Returns:
+        dict: The parsed tree structure as a dictionary.
+    """
+    try:
+        # Attempt to parse the tree string as JSON
+        tree_dict = json.loads(tree_str)
+    except json.JSONDecodeError:
+        # If JSON parsing fails, try to parse it as Python literal
+        # This is useful for tree output with trailing commas
+        tree_dict = ast.literal_eval(tree_str)
+        warnings.warn(
+            "Tree output has trailing commas, using ast.literal_eval instead of json.loads"
+        )
+    return tree_dict
+
+
+def get_disk_usage(tree_dict, pipeline_outdir):
+    try:
+        report = next(
+            (
+                item
+                for item in tree_dict
+                if isinstance(item, dict) and item.get("type") == "report"
+            )
+        )
+        dir_size = report.get("size", math.nan)
+    except StopIteration:  # occurs when there is no report in the tree dict
+        dir_size = math.nan
+    finally:
+        if math.isnan(dir_size):
+            # Fallback to using du command to determine disk usage
+            warnings.warn(
+                "Report or size not found in tree, using du to determine disk usage"
+            )
+            dir_size = run_du(pipeline_outdir)
+    return dir_size
+
+
+def run_du(dirpath):
+    """
+    Calculates the total size of a directory in bytes using the `du` shell command.
+
+    Args:
+        dirpath (str): Path to the directory whose size is to be calculated.
+    Returns:
+        int or float: The size of the directory in bytes. Returns NaN if the size cannot be determined.
+    Raises:
+        Issues a warning if the directory size cannot be parsed or if the `du` command fails.
+    """
+    try:
+        dir_size = int(shell_run(f"du -bs {dirpath}").split("\t")[0])
+    except Exception as e:
+        warnings.warn(
+            f"Error parsing directory size with du command for {dirpath}:\n{e}"
+        )
+        dir_size = math.nan
+    return dir_size
 
 
 def glob_files(
