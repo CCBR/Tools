a222f5f8c56baa2cc0593e2f946bc238f7590887
Kelly Sovacool <kelly.sovacool@nih.gov>
2025-05-14 12:56:39 -0400

    feat: count nsamples for all pipelines but escape & logan

---
+++ src/ccbr_tools/paths.py
@@ -0,0 +1,146 @@
+import ast
+import glob
+import json
+import math
+import pathlib
+import tarfile
+import warnings
+
+
+def get_tree(pipeline_outdir, args="-aJ --du"):
+    """
+    Generate a directory tree structure using the `tree` command-line utility
+
+    Note: when using -J with --du, the output is not valid JSON due to extra trailing commas.
+    It can be parsed with `ast.literal_eval` rather than `json.loads`.
+
+    Args:
+        pipeline_outdir (str): The path to the directory for which the tree
+            structure will be generated.
+        args (str, optional): Additional arguments to pass to the `tree`
+            command. Defaults to "-aJ" for including hidden files and formatting output as JSON
+
+    Returns:
+        str: The directory tree structure as a string, stripped of any
+        leading or trailing whitespace.
+    """
+    return shell_run(f"tree {args} {pipeline_outdir}").strip()
+
+
+def load_tree(tree_str):
+    """
+    Load a tree structure from a string, attempting to parse it as JSON or
+    Python literal.
+
+    Args:
+        tree_str (str): The string representation of the tree structure.
+    Returns:
+        dict: The parsed tree structure as a dictionary.
+    """
+    try:
+        # Attempt to parse the tree string as JSON
+        tree_dict = json.loads(tree_str)
+    except json.JSONDecodeError:
+        # If JSON parsing fails, try to parse it as Python literal
+        # This is useful for tree output with trailing commas
+        tree_dict = ast.literal_eval(tree_str)
+        warnings.warn(
+            "Tree output has trailing commas, using ast.literal_eval instead of json.loads"
+        )
+    return tree_dict
+
+
+def get_disk_usage(tree_dict, pipeline_outdir):
+    try:
+        report = next(
+            (
+                item
+                for item in tree_dict
+                if isinstance(item, dict) and item.get("type") == "report"
+            )
+        )
+        dir_size = report.get("size", math.nan)
+    except StopIteration:  # occurs when there is no report in the tree dict
+        dir_size = math.nan
+    finally:
+        if math.isnan(dir_size):
+            # Fallback to using du command to determine disk usage
+            warnings.warn(
+                "Report or size not found in tree, using du to determine disk usage"
+            )
+            dir_size = run_du(pipeline_outdir)
+    return dir_size
+
+
+def run_du(dirpath):
+    """
+    Calculates the total size of a directory in bytes using the `du` shell command.
+
+    Args:
+        dirpath (str): Path to the directory whose size is to be calculated.
+    Returns:
+        int or float: The size of the directory in bytes. Returns NaN if the size cannot be determined.
+    Raises:
+        Issues a warning if the directory size cannot be parsed or if the `du` command fails.
+    """
+    try:
+        dir_size = int(shell_run(f"du -bs {dirpath}").split("\t")[0])
+    except Exception as e:
+        warnings.warn(
+            f"Error parsing directory size with du command for {dirpath}:\n{e}"
+        )
+        dir_size = math.nan
+    return dir_size
+
+
+def glob_files(
+    pipeline_outdir,
+    patterns=[
+        "snakemake.log",
+        ".nextflow.log",
+        "*.jobby*",
+        "master.log",
+        "runtime_statics*",
+    ],
+):
+    """
+    Collects files from a specified directory and its subdirectories that match a list of patterns.
+
+    Args:
+        pipeline_outdir (str): The base directory to search for files.
+        patterns (list of str, optional): A list of glob patterns to match files. Defaults to:
+            [
+                "snakemake.log",
+                ".nextflow.log",
+                "*.jobby*",
+                "master.log",
+                "runtime_statics*",
+            ].
+
+    Returns:
+        set of pathlib.Path: A set of `pathlib.Path` objects representing the matched files.
+    """
+    return {
+        pathlib.Path(f)
+        for pattern in patterns
+        for f in itertools.chain(
+            glob.glob(
+                f"{pipeline_outdir}/{pattern}",
+            ),
+            glob.glob(f"{pipeline_outdir}/**/{pattern}"),
+        )
+        if pathlib.Path(f).is_file()
+    }
+
+
+def create_tar_archive(files, tar_filename):
+    """
+    Creates a compressed tar archive (.tar.gz) containing the specified files.
+
+    Args:
+        files (list of pathlib.Path): A list of file paths to include in the archive.
+        tar_filename (str): The name of the output tar.gz file.
+    """
+    with tarfile.open(tar_filename, "w:gz") as tar:
+        for file in files:
+            tar.add(file, arcname=file.name)
--- src/ccbr_tools/pipeline/__init__.py
+++ src/ccbr_tools/pipeline/__init__.py
@@ -9,51 +9,7 @@ Helpers for bioinformatics pipelines
 
 import math
 import re
-
-
-class Pipeline:
-    SAMPLES_PATTERN = None
-
-    def count_samples(self, tree_str):
-        """
-        Count the number of samples in a pipeline run.
-
-        Args:
-            tree_str (str): The tree as a JSON string representing the pipeline run output directory (from `tree -aJ` command).
-            pipeline_name (str): The name of the pipeline.
-
-        Returns:
-            int: The number of samples in the pipeline run. Returns NaN if the pipeline is not recognized.
-
-        See Also:
-            `~ccbr_tools.spooker.get_tree`: The function used to generate the tree structure.
-        """
-        nsamples = math.nan
-        try:
-            nsamples = re.findall(self.SAMPLES_PATTERN, tree_str)
-        except Exception as err:
-            warnings.warn(
-                f"Could not determine number of samples for pipeline {pipeline_name}. See original error message below:\n{repr(err)}"
-            )
-        return nsamples
-
-
-class RENEE(Pipeline):
-    SAMPLES_PATTERN = r"Aligned.toTranscriptome.out.bam"
-
-
-class XAVIER(Pipeline):
-    SAMPLES_PATTERN = r".input.bam"
-
-
-PIPELINES = {
-    "renee": RENEE,
-    "xavier": XAVIER,
-}
-
-
-def create_pipeline(pipeline_name, pipelines=PIPELINES):
-    return pipelines.get(pipeline_name.lower(), None)
+from ..paths import load_tree
 
 
 def count_samples(tree_str, pipeline_name):
@@ -76,5 +32,90 @@ def count_samples(tree_str, pipeline_name):
         nsamples = pipeline.count_samples(tree_str)
     else:
         warnings.warn(f"Pipeline {pipeline_name} not recognized. Cannot count samples.")
-
     return nsamples
+
+
+class Pipeline:
+    SAMPLES_PATTERN = None  # must be a regex pattern with exactly one capture group, which excludes slashes
+
+    def count_samples(self, tree_str):
+        """
+        Count the number of samples in a pipeline run.
+
+        Args:
+            tree_str (str): The tree as a JSON string representing the pipeline run output directory (from `tree -aJ` command).
+            pipeline_name (str): The name of the pipeline.
+
+        Returns:
+            int: The number of samples in the pipeline run. Returns NaN if the pipeline is not recognized.
+
+        See Also:
+            `~ccbr_tools.spooker.get_tree`: The function used to generate the tree structure.
+        """
+        nsamples = math.nan
+        try:
+            # get unique set of capture groups for all matches
+            nsamples = len(set(re.findall(self.SAMPLES_PATTERN, tree_str)))
+        except Exception as err:
+            warnings.warn(
+                f"Could not determine number of samples for pipeline {pipeline_name}. See original error message below:\n{repr(err)}"
+            )
+        return nsamples
+
+
+class ASPEN(Pipeline):
+    SAMPLES_PATTERN = r'"name":"([a-zA-Z0-9_\-\.]*.reads.bam.bai)"'
+
+
+class CARLISLE(Pipeline):
+    SAMPLES_PATTERN = r'"name":"([a-zA-Z0-9_\-\.]*no_dedup.bam.bai)"'
+
+
+class CHAMPAGNE(Pipeline):
+    SAMPLES_PATTERN = r'"name":"([a-zA-Z0-9_\-]*filtered.sorted.bam.bai)"'
+
+
+class CHARLIE(Pipeline):
+    SAMPLES_PATTERN = r'"name":"([a-zA-Z0-9_\-\.]*.ciri.bam.csi)"'
+
+
+class CRISPIN(Pipeline):
+    SAMPLES_PATTERN = r'"name":"([a-zA-Z0-9_\-\.]*).foldchange.txt"'
+
+
+class ESCAPE(Pipeline):
+    SAMPLES_PATTERN = None  # TODO
+
+
+class LOGAN(Pipeline):
+    SAMPLES_PATTERN = None  # TODO
+
+
+class RENEE(Pipeline):
+    SAMPLES_PATTERN = r'"name":"([a-zA-Z0-9_\-\.]*Aligned.toTranscriptome.out.bam)"'
+
+
+class SINCLAIR(Pipeline):
+    SAMPLES_PATTERN = r'"name":"([a-zA-Z0-9_\-\.]*seurat_preprocess.rds)"'
+
+
+class XAVIER(Pipeline):
+    SAMPLES_PATTERN = r'"name":"([a-zA-Z0-9_\-\.]*.input.bam)"'
+
+
+PIPELINES = {
+    "aspen": ASPEN,
+    "carlisle": CARLISLE,
+    "champagne": CHAMPAGNE,
+    "charlie": CHARLIE,
+    "crispin": CRISPIN,
+    "escape": ESCAPE,
+    "logan": LOGAN,
+    "renee": RENEE,
+    "sinclair": SINCLAIR,
+    "xavier": XAVIER,
+}
+
+
+def create_pipeline(pipeline_name, pipelines=PIPELINES):
+    return pipelines.get(pipeline_name.lower(), None)
